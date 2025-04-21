84bc2ea9a340569312bafb52fa65c8253b182eac
nakovalskaya <na.kovalskaya@yandex.ru>
2025-04-21 12:07:37 +0300

    выва

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,14 +21,14 @@ export default function RootLayout({
   return (
     <html lang="en">
   
-        {/* Подключаем Telegram WebApp SDK корректно */}
-        <head>
-  <Script
-    src="https://telegram.org/js/telegram-web-app.js"
-    strategy="beforeInteractive"
-    data-auth-data={process.env.NEXT_PUBLIC_TG_BOT_AUTH_DATA}
-  />
-</head>
+  <head>
+        {/* ✅ Подключаем Telegram WebApp SDK корректно */}
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+          data-auth-data={process.env.NEXT_PUBLIC_TG_BOT_AUTH_DATA}
+        />
+      </head>
       <body className={inter.className}>
         <div style={{ width: '100vw', height: '100vh', overflow: 'auto' }}>
           {children}

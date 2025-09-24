a4beec11df063b723661529eb5fb17a3bd254ee8
nakovalskaya <na.kovalskaya@yandex.ru>
2025-09-24 19:37:13 +0300

    ориор

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function BottomNav() {
   const currentPath = usePathname();
@@ -22,12 +23,13 @@ export default function BottomNav() {
         const isActive = currentPath === href;
 
         return (
-          <a
+          <Link
             key={label}
             href={href}
             className={`flex flex-col items-center justify-end w-16 
                         text-[10px] select-none touch-manipulation
                         ${isActive ? 'text-[#EBDEC8]' : 'text-[#665d61] hover:text-white'}`}
+            draggable={false}
           >
             <div className="relative w-10 h-10 flex items-center justify-center translate-y-1">
               {/* Градиентная подсветка */}
@@ -41,7 +43,7 @@ export default function BottomNav() {
               {/* Светящаяся аура снизу */}
               <div
                 className={`absolute -bottom-1 left-1/2 -translate-x-1/2 
-                w-8 h-3 bg-[#991428]/20 rounded-full blur-lg z-0
+                            w-8 h-3 bg-[#991428]/20 rounded-full blur-lg z-0
                             transition-opacity duration-200
                             ${isActive ? 'opacity-100' : 'opacity-0'}`}
               />
@@ -63,7 +65,7 @@ export default function BottomNav() {
             <span className="mt-[2px] transition-colors duration-200">
               {label}
             </span>
-          </a>
+          </Link>
         );
       })}
     </nav>
